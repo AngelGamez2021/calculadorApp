b89014f082fc402531b5d3c844b392d7525d2dc9
Angel Gamez <agamez6@unisimon.edu.co>
2024-10-16 20:41:06 -0500

    logica-inicial

---
+++ src/app/components/dispositivo/dispositivo.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dispositivo',
+  templateUrl: './dispositivo.component.html',
+  styleUrls: ['./dispositivo.component.scss']
+})
+export class DispositivoComponent {
+
+  displayValue = '';      // El valor que se muestra en la pantalla
+  currentValue = '';      // El número actual
+  previousValue = '';     // El número previo antes de seleccionar una operación
+  operation = '';         // Operación actual (+, -, *, /)
+
+
+  isDarkMode = false;
+
+  constructor() { }
+
+  // Añadir un número al display
+  appendNumber(number: string) {
+    this.displayValue += number;
+    this.currentValue += number;
+  }
+
+  // Asignar operación
+  setOperation(operation: string) {
+    if (this.currentValue === '') return; // No permitir operaciones sin un valor
+    this.operation = operation;
+    this.previousValue = this.currentValue;
+    this.currentValue = '';
+    this.displayValue += ` ${operation} `;
+  }
+
+  // Limpiar la pantalla
+  clear() {
+    this.displayValue = '';
+    this.currentValue = '';
+    this.previousValue = '';
+    this.operation = '';
+  }
+
+
+
+  // Realizar la operación
+  compute() {
+    let result: number;
+    const prev = parseFloat(this.previousValue);
+    const current = parseFloat(this.currentValue);
+
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (this.operation) {
+      case '+':
+        result = prev + current;
+        break;
+      case '-':
+        result = prev - current;
+        break;
+      case '*':
+        result = prev * current;
+        break;
+      case '/':
+        result = prev / current;
+        break;
+      default:
+        return;
+    }
+
+    this.displayValue = result.toString();
+    this.currentValue = result.toString();
+    this.operation = '';
+    this.previousValue = '';
+  }
+
+ 
+}
